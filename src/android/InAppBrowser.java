d59178549f2bf1d9fcf66ff1428c07c00a5bc62e
rc <arseny@listokcrm.ru>
2023-02-26 13:37:11 +0100

    ups

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -1216,6 +1216,7 @@ public class InAppBrowser extends CordovaPlugin {
                 }
             }else if(url.startsWith("intent:")) {
                 try {
+                            LOG.e(LOG_TAG, "INTENTING this " + e.toString() );
                             Intent intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME);
                                 if (intent.resolveActivity(packageManager) != null) {
                                     cordova.getActivity().startActivity(intent);
